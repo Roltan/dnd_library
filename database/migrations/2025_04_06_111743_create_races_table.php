a724af24d3a5d8f03bc33d8c2c9ac8a260cad969
Roltan <KhalikoR@yandex.ru>
2025-04-06 20:41:36 +0500

    рассы

---
+++ database/migrations/2025_04_06_111743_create_races_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('races', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('manual')->default('https://dnd.su/race/');
+            $table->string('source')->default("Player's handbook");
+            $table->integer('speed');
+            $table->string('size')->default("Средний"); // размер существа
+            $table->integer('height'); // средний рост взрослых особей
+            $table->integer('weight'); // средний вес взрослых особей
+            $table->json('lang'); // массив доступных языков
+            $table->json('ideology'); // массив доступных идеологий
+            $table->json('abilities_bonus')->nullable(); // увеличение характеристик
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('races');
+    }
+};
--- database/migrations/2025_04_06_125031_create_skills_table.php
+++ database/migrations/2025_04_06_125031_create_skills_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('klass_id')->constrained('klasses')->cascadeOnDelete();
-            $table->foreignId('sub_klass_id')->nullable()->constrained('sub_klasses')->cascadeOnDelete();
+            $table->bigInteger('caster_id')->unsigned();
+            $table->string('table');
             $table->string('name');
             $table->string('description');
             $table->integer('lvl');
