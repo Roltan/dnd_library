cf8fe7f4e685889b791fd3ff6845021ae34be9d8
Roltan <KhalikoR@yandex.ru>
2025-04-06 18:14:20 +0500

    миграции таблиц классов

---
+++ database/migrations/2025_04_06_115639_create_klasses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('klasses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('manual')->default('https://dnd.su/class/');
+            $table->string('source')->default("Player's handbook");
+            $table->integer('dice_hp');
+            $table->json('proficiency'); // массив владений доспехами, оружием, инструментами, языками
+            $table->json('save_stat'); // массив владений спас бросками
+            $table->json('abilities'); // массив навыков которые можно выбрать при создании
+            $table->integer('abilities_count'); // сколько навыков можно взять
+            $table->json('equipment'); // массив вариантов доступного снаряжения на старте
+            $table->string('money'); // монеты вместо снаряжения
+
+            $table->string('abilities_spell')->nullable(); // заклинательная характеристика
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('klasses');
+    }
+};
---
+++ database/migrations/2025_04_06_125031_create_skills_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('klass_id')->constrained('klasses')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('lvl');
+            $table->string('script')->nullable();
+            $table->boolean('set_price'); // ограничено ли применение способности
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('skills');
+    }
+};
